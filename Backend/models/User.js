2ec08228ffd438e5e7f17a779dbbfcbfb0f03659
clientsmaker <interiorclientsmaker@gmail.com>
2025-07-12 16:38:52 +0530

    user model modified

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
-phone: { type: String, required: true, trim: true },
+phone: { type: String,  trim: true },
     // ✅ Role-based Access
     role: { type: String, enum: ["admin", "user", "customer"], default: "user" },
 
